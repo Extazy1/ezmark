fd63c4d5a97235c6ff63e5388a51b17a75608887
Extazy <2056345020@qq.com>
2025-10-20 10:55:23 +0800

    fix dashboard name error

--- ezmark-client-master/src/app/dashboard/page.tsx
+++ ezmark-client-master/src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 // 防止被静态预渲染（可选但推荐）
 export const dynamic = "force-dynamic";
 
-// 关键：客户端加载，跳过 SSR，避免 Node 环境访问到 File
+// 关键：客户端加载，跳过 SSR，避免 Node 环境访问到 File 小写
 const DashboardClient = dynamic(() => import("./DashboardClient"), {
   ssr: false,
 });
