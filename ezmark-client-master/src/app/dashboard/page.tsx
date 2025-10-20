b9db6a085deda57e969f0574cf3d1726524dafd6
Extazy <2056345020@qq.com>
2025-10-20 11:02:32 +0800

    fix type error

--- ezmark-client-master/src/app/dashboard/page.tsx
+++ ezmark-client-master/src/app/dashboard/page.tsx
@@ -1,10 +1,6 @@
-// app/dashboard/page.tsx
+// src/app/dashboard/page.tsx
 import dynamic from "next/dynamic";
 
-// 防止被静态预渲染（可选但推荐）
-export const dynamic = "force-dynamic";
-
-// 关键：客户端加载，跳过 SSR，避免 Node 环境访问到 File 小写
 const DashboardClient = dynamic(() => import("./DashboardClient"), {
   ssr: false,
 });
